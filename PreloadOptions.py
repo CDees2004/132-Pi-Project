8bd5ed45043f552cd19306ab3041b730f504aec7
CDees2004 <CDees2004@outlook.com>
2024-02-03 20:49:03 -0600

    primitive version of UI used for anim selection for preloads

--- PreloadOptions.py
+++ PreloadOptions.py
@@ -6,18 +6,11 @@
 
 # import the needed libraries 
 from tkinter import *
-import os 
 
-# function that launches animation software when called
-
-def openPreBuilt():
-    os.execvp("python", ["python", "AnimationFoldTest.py"])
-
-def openImageCapture():
-    os.execvp("python", ["python", "CameraTesting.py"])
+class InputHolder:
     
-def openSettings():
-    os.execvp("python", ["python", "UserSettings.py"])
+    UserInput = None
+    
 
 # Create the main Tkinter window
 
@@ -26,17 +19,23 @@ class App(Frame):
     def __init__(self, master):
         Frame.__init__(self, master, bg="blue")  # Set background color
 
-        self.button1 = Button(master, text="------ Anim.", fg="black", bg="lightblue", bd=2, height=3, font=("Helvetica", 14, "bold"), command = openImageCapture)
+        self.button1 = Button(master, text="Gojo Anim.", fg="black", bg="lightblue", bd=2, height=3, font=("Helvetica", 14, "bold"), command = self.change_input_gojo)
         self.button1.pack(side=TOP, fill=X, pady=5)
 
-        self.button2 = Button(master, text="------ Anim.", fg="white", bg="red", height=3, font=("Helvetica", 14, "bold"), command = openPreBuilt)
+        self.button2 = Button(master, text="Sukuna Anim.", fg="white", bg="red", height=3, font=("Helvetica", 14, "bold"), command = self.change_input_sukuna)
         self.button2.pack(side=TOP, fill=BOTH, pady=5)
 
-        self.button3 = Button(master, text="------ Anim.", fg="white", bg="darkred", height=3, font=("Helvetica", 14, "bold"), command = openSettings)
-        self.button3.pack(side=TOP, fill=BOTH, pady=5)
+        #self.button3 = Button(master, text="------ Anim.", fg="white", bg="darkred", height=3, font=("Helvetica", 14, "bold"), command = openSettings)
+        #self.button3.pack(side=TOP, fill=BOTH, pady=5)
 
-        self.button4 = Button(master, text="------ Anim.", fg="white", bg="green", height=3, font=("Helvetica", 14, "bold"), command = self.quit)
-        self.button4.pack(side=TOP, fill=BOTH, pady=5)
+        #self.button4 = Button(master, text="------ Anim.", fg="white", bg="green", height=3, font=("Helvetica", 14, "bold"), command = self.quit)
+        #self.button4.pack(side=TOP, fill=BOTH, pady=5)
+        
+    def change_input_gojo(self):
+        InputHolder.UserInput = "Gojo"
+        
+    def change_input_sukuna(self):
+        InputHolder.UserInput = "Sukuna"
 
         
 
