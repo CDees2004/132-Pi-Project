39a8a5532296bab493dc01313c162a9d1b78a14b
CDees2004 <CDees2004@outlook.com>
2024-02-05 10:27:29 -0600

    GUI that lets users choose which animation will play

--- PreloadOptions.py
+++ PreloadOptions.py
@@ -7,6 +7,9 @@
 # import the needed libraries 
 from tkinter import *
 
+def close_window():
+    window.destroy()
+
 class InputHolder:
     
     UserInput = None
@@ -25,8 +28,8 @@ class App(Frame):
         self.button2 = Button(master, text="Sukuna Anim.", fg="white", bg="red", height=3, font=("Helvetica", 14, "bold"), command = self.change_input_sukuna)
         self.button2.pack(side=TOP, fill=BOTH, pady=5)
 
-        #self.button3 = Button(master, text="------ Anim.", fg="white", bg="darkred", height=3, font=("Helvetica", 14, "bold"), command = openSettings)
-        #self.button3.pack(side=TOP, fill=BOTH, pady=5)
+        self.button3 = Button(master, text="Confirm", fg="white", bg="darkred", height=3, font=("Helvetica", 14, "bold"), command = close_window)
+        self.button3.pack(side=TOP, fill=BOTH, pady=5)
 
         #self.button4 = Button(master, text="------ Anim.", fg="white", bg="green", height=3, font=("Helvetica", 14, "bold"), command = self.quit)
         #self.button4.pack(side=TOP, fill=BOTH, pady=5)
