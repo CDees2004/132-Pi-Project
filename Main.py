97da5e9b59e42e80ca1a4bbaea18f8ed74b27500
CDees2004 <CDees2004@outlook.com>
2024-02-05 12:15:53 -0600

    converting UserGUI to CustomTkinter

--- Main.py
+++ Main.py
@@ -5,35 +5,57 @@
 from customtkinter import *
 from PIL import Image, ImageTk
 
+# initializing the Ctk Window and its size 
+app = CTk()
+app.geometry("800x600")         # size variable to change on pi 
+app.title("Animaster")
+
 # settings the appearance 
 set_appearance_mode("dark")
 set_default_color_theme("blue")
 
-
 # functions used to execute the other programs from this window 
 def openPreBuilt():
     os.execvp("python", ["python", "AnimRunTesting.py"])
+    
+def openImageCapture():
+    os.execvp("python", ["python", "ImageCaptureFINAL.py"])
+
+#############################################################################################
+
+frame_1 = CTkFrame(master=app)
+frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
 
-# initializing the Ctk Window and its size 
-app = CTk()
-app.geometry("800x600")         # size variable to change on pi 
+# label that serves as the title for the window 
+label_title = CTkLabel(master = app, text = "Animaster", text_color = "black",  width = 2000,
+                       height = 2, font = ("Century Gothic", 60), corner_radius = 8, anchor = "center", 
+                       bg_color = "white")
 
-label_title = CTkLabel(master = app, text = "Main Title", width = 120, height = 25, corner_radius = 8)
 label_title.place(relx = 0.5, rely = 0.1, anchor = "center")
 
-btn = CTkButton(master = app , text = "Click me", corner_radius = 32, fg_color = "white", border_color = "blue",
-                border_width = 2, text_color = "black", command = openPreBuilt)
+# label with the phrase on it 
 
-my_image = CTkImage(Image.open("bgtest.JPG"), size = (26, 26))
+label_phrase = CTkLabel(master = frame_1, text = "You Are the Master of Your Own Creation", text_color = "white", 
+                        width = 120, height = 1, font = ("Century Gothic", 20), corner_radius = 8, anchor = "center")
 
-button_image = CTkButton(app, image = my_image)
+label_phrase.place(relx = 0.5, rely = 0.19, anchor = "center")
 
-#btn.pack()
-btn.place(relx = 0.5, rely = 0.2, anchor = "center")
+# button to launch the preloaded animation library 
+btn = CTkButton(master = frame_1 , text = "Animation Library", corner_radius = 32, fg_color = "white", border_color = "blue",
+                border_width = 2, text_color = "black", height = 50, width = 800, hover_color= "lavender", font = ("Century Gothic", 20),
+                command = openPreBuilt)
 
-ent = CTkEntry(master = app)
-ent.place(relx = 0.5, rely = 0.3, anchor = "center")
+btn.place(relx = 0.5, rely = 0.3, anchor = "center")
+
+
+# button to launch the image capture software
+btn2 = CTkButton(master = frame_1 , text = "Image Capture", corner_radius = 32, fg_color = "white", border_color = "blue",
+                border_width = 2, text_color = "black", height = 50, width = 800, hover_color= "lavender", font = ("Century Gothic", 20),
+                command = openImageCapture)
+
+btn2.place(relx = 0.5, rely = 0.4, anchor = "center")
+
 
 app.mainloop()
 # keeping this window open 
