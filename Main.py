44f39a1fc899590028ecfcef2cd178ad662e46dd
CDees2004 <CDees2004@outlook.com>
2024-02-07 12:29:04 -0600

    made changes to CTk appearance

--- Main.py
+++ Main.py
@@ -27,57 +27,54 @@ class App(CTk):
 
         # the frame which will contain the elements of the GUi 
         self.frame_1 = CTkFrame(master=self)
-        self.frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+        self.frame_1.pack(pady = 20, padx = 60, fill = "both", expand = True)
 
         # label for the title of the program 
-        self.label_title = CTkLabel(master=self.frame_1, text="Animaster", text_color="black", width=2000,
-                                     height=2, font=("Constantia", 60), corner_radius=8, anchor="center", 
-                                     bg_color="white")
-        self.label_title.place(relx=0.5, rely=0.1, anchor="center")
+        self.label_title = CTkLabel(master = self.frame_1, text = "Animaster", text_color = "black", width = 2000,
+                                     height = 2, font = ("Constantia", 60), corner_radius = 8, anchor = "center", 
+                                     bg_color = "white")
+        self.label_title.place(relx = 0.5, rely = 0.1, anchor = "center")
 
         # label for the phrase associated with the program 
-        self.label_phrase = CTkLabel(master=self.frame_1, text="You Are the Master of Your Own Creation", text_color="white", 
-                                     width=120, height=1, font=("Constantia", 20), corner_radius=8, anchor="center")
-        self.label_phrase.place(relx=0.5, rely=0.19, anchor="center")
+        self.label_phrase = CTkLabel(master = self.frame_1, text = "You Are the Master of Your Own Creation", text_color = "white", 
+                                     width = 120, height = 1, font = ("Constantia", 20), corner_radius = 8, anchor = "center")
+        self.label_phrase.place(relx = 0.5, rely = 0.19, anchor = "center")
 
         # button for launching the preloaded animation library
-        self.btn = CTkButton(master=self.frame_1, text="Animation Library", corner_radius=32, fg_color="white", border_color="blue",
-                             border_width=2, text_color="black", height=50, width=200, hover_color="light blue", font=("Century Gothic", 20),
-                             command=self.openPreBuilt)
-        self.btn.place(relx=0.2, rely=0.5, anchor="center")
+        self.btn = CTkButton(master = self.frame_1, text = "Animation Library", corner_radius = 32, fg_color = "white", border_color = "blue",
+                             border_width = 2, text_color = "black", height = 50, width = 300, hover_color = "light blue", font = ("Constantia", 20),
+                             command = self.openPreBuilt)
+        self.btn.place(relx = 0.25, rely = 0.6, anchor = "center")
 
         # button for launching the image capture software
-        self.btn2 = CTkButton(master=self.frame_1, text="Image Capture", corner_radius=32, fg_color="white", border_color="blue",
-                              border_width=2, text_color="black", height=50, width=50, hover_color="light blue", font=("Century Gothic", 20),
-                              command=self.openImageCapture)
-        self.btn2.place(relx=0.8, rely=0.5, anchor="center")
+        self.btn2 = CTkButton(master = self.frame_1, text = "Image Capture", corner_radius = 32, fg_color = "white", border_color = "blue",
+                              border_width = 2, text_color = "black", height = 50, width = 300, hover_color = "light blue", font = ("Constantia", 20),
+                              command = self.openImageCapture)
+        self.btn2.place(relx = 0.75, rely = 0.6, anchor = "center")
 
         # button for exiting the software
-        self.btn3 = CTkButton(master=self.frame_1, text="Exit", corner_radius=32, fg_color="white", border_color="red",
-                              border_width=2, text_color="black", height=50, width=800, hover_color="pink", font=("Century Gothic", 20),
-                              command=self.quitProgram)
-        self.btn3.place(relx=0.5, rely=0.9, anchor="center")
+        self.btn3 = CTkButton(master = self.frame_1, text = "Exit", corner_radius = 32, fg_color = "white", border_color = "red",
+                              border_width = 2, text_color = "black", height = 50, width = 750, hover_color = "pink", font = ("Constantia", 20),
+                              command = self.quitProgram)
+        self.btn3.place(relx = 0.5, rely = 0.7, anchor = "center")
         
-        # Loading Image for GUI (above preloaded)
+        # preview Image for GUI (above preloaded)
         current_path = os.path.dirname(os.path.realpath(__file__))
         self.bg_image = CTkImage(Image.open(current_path + "/ImageFolder/MickeyModelSheet.jpg"),
-                                               size=(150, 125))
+                                               size = (300, 180))
         self.bg_image_label = CTkLabel(self, image=self.bg_image)
-        self.bg_image_label.place(relx = 0.15,rely = 0.25)
+        self.bg_image_label.place(relx = 0.1,rely = 0.25)
         
         
-        # Loading Image for GUI (above image capture)
+        # pewview Image for GUI (above image capture)
         current_path = os.path.dirname(os.path.realpath(__file__))
         self.bg_image = CTkImage(Image.open(current_path + "/ImageFolder/JujutsuKaisenGenga.jpg"),
-                                               size=(150, 125))
+                                               size = (300, 180))
         self.bg_image_label = CTkLabel(self, image=self.bg_image)
-        self.bg_image_label.place(relx = 0.66,rely = 0.25)
+        self.bg_image_label.place(relx = 0.53,rely = 0.25)
         
-
         
-
-
-    # Functions that allow this window to launch the other windows/ exit
+    # functions that allow this window to launch the other windows/ exit
     def openPreBuilt(self):
         os.execvp("python", ["python", "PreloadedAnimations.py"])
     
